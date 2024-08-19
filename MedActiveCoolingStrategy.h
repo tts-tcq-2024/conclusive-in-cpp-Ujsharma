580c31d7c427d43077ad8c0f86a30b1bd60ce047
Ujsharma <ujwalsharma.s@gmail.com>
2024-08-19 16:40:18 +0530

    Update MedActiveCoolingStrategy.h

--- MedActiveCoolingStrategy.h
+++ MedActiveCoolingStrategy.h
@@ -6,12 +6,6 @@
 class MedActiveCoolingStrategy : public CoolingStrategy {
 public:
     BreachType classifyTemperature(double temperatureInC) const override {
-        if (temperatureInC < 0) {
-            return TOO_LOW;
-        } else if (temperatureInC > 35) {
-            return TOO_HIGH;
-        }
-        return NORMAL;
         return inferBreach(temperatureInC, 0, 40);
     }
 };
